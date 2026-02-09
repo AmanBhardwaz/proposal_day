69e3653121c36ae4247044caf08c346aece0d232
AmanBhardwaz <aman92341242@gmail.com>
2026-02-09 13:47:46 +0530

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const CORRECT_PASSWORD = 'oh my darling I love you';
+const CORRECT_PASSWORD = 'I love you Trijya';
 
 // DOM Elements
 const passwordForm = document.getElementById('passwordForm');
